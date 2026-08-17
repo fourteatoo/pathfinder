80afb339f17a0384f745aaa79710222e9e034405
Walter Pelissero <walter@pelissero.de>
2026-08-17 18:55:39 +0200

    Fix visibility of trend-icon

--- src/cljs/pathfinder/util.cljs
+++ src/cljs/pathfinder/util.cljs
@@ -66,7 +66,7 @@
       [:path {:stroke-linecap "round" :stroke-linejoin "round" 
               :d "M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"}]]]))
 
-(defn- trend-icon [trend-kw]
+(defn trend-icon [trend-kw]
   (case trend-kw
     :up   [:span {:style {:color "#16a34a" :margin-left "4px"}} "▲"]
     :down [:span {:style {:color "#dc2626" :margin-left "4px"}} "▼"]
